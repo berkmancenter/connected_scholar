ac7cf840f4cad065103173dea2f44e466cfd0014
Matt Smith <matt@mattsmith.me>
2011-08-17 06:59:24 -0500

    install / initialize cucumber and rspec

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module ConnectedScholar
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -39,5 +41,10 @@ module ConnectedScholar
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.generators do |g|
+      g.test_framework :rspec, :views => false
+    end
+    
   end
 end
