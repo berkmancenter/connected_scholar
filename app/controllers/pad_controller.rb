ca4ce53c967347c9b2ce7425274e272e5d1bba36
Matt Smith <matt@mattsmith.me>
2011-08-18 06:51:07 -0500

    added support for exporting of pads via http_client

--- app/controllers/pad_controller.rb
+++ app/controllers/pad_controller.rb
@@ -8,12 +8,27 @@ class PadController < ApplicationController
   end
   
   def read_only
-    @pad_id = params[:id]
+    pad_id = params[:id]
     html = nil
     with_etherpad_url do |url|
       http = HTTPClient.new
-      html = http.get_content("#{url}/ro/#{@pad_id}")
+      html = http.get_content("#{url}/ro/#{pad_id}")
     end
-    render :text => html, :content_type => 'text/html'
+    render :text => html
+  end
+  
+  def export
+    type = params[:type]
+    if ["txt", "html"].include?(type)
+      pad_id = params[:pad_id]
+      output = nil
+      with_etherpad_url do |url|
+        http = HTTPClient.new
+        output = http.get_content("#{url}/p/#{pad_id}/export/#{type}")
+      end
+      render :text => output
+    else
+      render :text => "Unsupported type."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ ConnectedScholar::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  match 'pad/' => 'pad#index'
-  match 'pad/pad' => 'pad#pad'
-  match 'p/:id' => 'pad#pad'
-  match 'ro/:id' => 'pad#read_only'
+  match 'pad/'                      => 'pad#index'
+  match 'pad/pad'                   => 'pad#pad'
+  match 'p/:id'                     => 'pad#pad'
+  match 'ro/:id'                    => 'pad#read_only'
+  match 'p/:pad_id/export/:type'    => 'pad#export'
   
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
