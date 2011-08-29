aadc406e2f00d6f0da79e5f7e89806e3494c3487
Matt Smith <matt@mattsmith.me>
2011-08-29 12:28:59 -0500

    Revert 4771c67bc5a4c344d32ec64d59379ed151f06301^..HEAD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  include EtherpadUtil
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,13 +9,4 @@ class User < ActiveRecord::Base
 
   #validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
-
-  def author_id
-    @author_id ||= create_author_if_not_exists_for(self)
-  end
-
-  def author_id=(value)
-    @author_id = value
-  end
-
 end
--- lib/etherpad_util.rb
+++ lib/etherpad_util.rb
@@ -1,6 +1,3 @@
-require 'rest_client'
-require 'json'
-
 module EtherpadUtil
 
   def with_etherpad_server
@@ -21,17 +18,6 @@ module EtherpadUtil
     end
   end
 
-  def create_author_if_not_exists_for(user)
-     with_etherpad_url do |url|
-       with_apikey do |apikey|
-         get_json "#{url}/api/1/createAuthorIfNotExistsFor?apikey=#{apikey}&authorMapper=#{user.id}&name=#{user.name}" do |data|
-           authorID = data["data"]["authorID"]
-           user.author_id = authorID
-         end
-       end
-     end
-  end
-
   private
 
   def with_etherpad
@@ -44,36 +30,5 @@ module EtherpadUtil
     global_yaml = YAML.load_file("#{Rails.root}/config/etherpad.global.yml")
     yield local_yaml['protocol'], local_yaml['host'], local_yaml['port'], local_yaml['path'], global_yaml['git'], global_yaml['ref']
     true
-  end
-
-  def with_apikey
-    unless File.exists?('vendor/etherpad-lite/APIKEY.txt')
-      puts "Cannot find vendor/etherpad-lite/APIKEY.txt.  Did you run 'rake etherpad:run'"
-      exit(1)
-    end
-
-    apikey = ""
-    File.open("#{Rails.root}/vendor/etherpad-lite/APIKEY.txt") do |f|
-      apikey = f.readline.strip
-    end
-    yield apikey
-  end
-  #need to move this to a common util lib once library cloud is committed
-  def get_json(url)
-    RestClient.get url, {
-        :content_type => :json,
-        :accept => :json,
-        :cache_control => "no-cache"
-    } do |response|
-      if response and !response.blank? and response != 'null'
-        if block_given?
-          yield JSON.parse(response)
-        else
-          return JSON.parse(response)
-        end
-      else
-        return nil
-      end
-    end
   end
 end
