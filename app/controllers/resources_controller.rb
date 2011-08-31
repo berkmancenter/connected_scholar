29ba61a93fb37957f72b479641ce67f4994c3d07
Joe Kutner <jpkutner@gmail.com>
2011-08-31 15:42:22 -0500

    Improved the controller action for creating a resource so that is accepts an item_id.  This is used to retrieve data from the LibraryCloud API and turn it into a resource.  Add an AJAX call on the frontend

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,8 +1,30 @@
 class ResourcesController < ApplicationController
+  include LibraryCloudUtil
+
   def create
     @document = Document.find(params[:document_id])
-    @resource = @document.recommended_resources.create(params[:resource])
-    redirect_to @document
+    item_id = params["item_id"]
+    if item_id.nil?
+      @resource = @document.recommended_resources.create(params[:resource])
+      redirect_to @document
+    else
+
+      items = item_search(:id, item_id)
+      item = items['docs'] ? items['docs'][0] : nil
+
+      raise "No Item: #{item_id.inspect}" if item.nil? or item['id'] != item_id
+
+      @resource = @document.recommended_resources.create(
+          :title => item['title'],
+          :authors => (item['creator'] || []).join(", "),
+          :abstract => (item['desc_subject'] || []).join(", "),
+          :source => item['publisher']
+      )
+
+      respond_to do |format|
+        format.js {render :json => @resource}
+      end
+    end
   end
 
   def destroy
