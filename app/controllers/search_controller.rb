9e087f015f4a392593950654683167a8a1adf690
Joe Kutner <jpkutner@gmail.com>
2011-09-13 12:14:01 -0500

    improved advanced search with dialog

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,9 +12,16 @@ class SearchController < ApplicationController
 
     @items = item_search(params[:search_type], params[:query], filters)
 
-    respond_to do |format|
-      format.html {render :partial=>'search/results', :content_type => "text/html", :layout => false, :locals => {:searchresults => @items['docs']}}
-      format.js {render :json => @items['docs']}
+    if @items
+      respond_to do |format|
+        format.html {render :partial=>'search/results', :content_type => "text/html", :layout => false, :locals => {:searchresults => @items['docs']}}
+        format.js {render :json => @items['docs']}
+      end
+    else
+      respond_to do |format|
+        format.html {render :text => "There was a problem with the search!"}
+        format.js {render :json => []}
+      end
     end
   end
 
