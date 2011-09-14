2ce878efdc796ea1156426eb2ab4ad4f3ca0daf2
Joe Kutner <jpkutner@gmail.com>
2011-09-14 15:23:19 -0500

    improved error handling in the search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,11 +13,20 @@ class SearchController < ApplicationController
     @items = item_search(params[:search_type], params[:query], filters)
 
     if @items
-      respond_to do |format|
-        format.html {render :partial=>'search/results', :content_type => "text/html", :layout => false, :locals => {:searchresults => @items['docs']}}
-        format.js {render :json => @items['docs']}
+      if @items['docs']
+        respond_to do |format|
+          format.html {render :partial=>'search/results', :content_type => "text/html", :layout => false, :locals => {:searchresults => @items['docs']}}
+          format.js {render :json => @items['docs']}
+        end
+      else
+        Rails.logger.error("Search Error: #{@items.inspect}")
+        respond_to do |format|
+          format.html {render :text => (@items['errors'] || "There was a problem with the search! ")}
+          format.js {render :json => @items}
+        end
       end
     else
+      Rails.logger.error("Search Error: No response from server")
       respond_to do |format|
         format.html {render :text => "There was a problem with the search!"}
         format.js {render :json => []}
