4cb3f3ff13c4cdfc504ebf4b6d30e0ca1c0bcc24
Joe Kutner <jpkutner@gmail.com>
2011-09-21 16:11:14 -0500

    improved etherpad_password migration so it sets the default value (don't worry, i didn't push it yet).

--- db/migrate/20110921202933_add_etherpad_password_to_documents.rb
+++ db/migrate/20110921202933_add_etherpad_password_to_documents.rb
@@ -1,5 +1,13 @@
 class AddEtherpadPasswordToDocuments < ActiveRecord::Migration
-  def change
+  def up
     add_column :documents, :etherpad_password, :string, :length => 50
+
+    execute("select id from documents").each do |row|
+      execute "update documents set etherpad_password='#{SecureRandom.hex(13)}' where id=#{row['id']}"
+    end
+  end
+
+  def down
+    remove_column :documents, :etherpad_password
   end
 end
