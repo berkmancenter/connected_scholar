da3d5320581584940dabd65f6a7008437495a992
Joe Kutner <jpkutner@gmail.com>
2011-09-27 10:11:28 -0500

    added date formatting javascript so dates can be shown in client timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,27 @@ module ApplicationHelper
       flash[name].nil? ? html : "#{html}<div class=\"flash #{name}\">#{flash[name]}</div>"
     end
   end
+
+  def display_datetime(datetime, span_id)
+    # really should do this in the client timezone.  which means it would need to be done in JS
+    if datetime.to_date == Date.today
+      format = "h:MM TT"
+    else
+      format = "h:MM TT 'on' mmm d, yyyy"
+    end
+    <<HTML
+<script>
+    jQuery(document).ready(function() {
+      $("##{span_id}").append(((new Date(Date.UTC(
+          #{datetime.year},
+          #{datetime.month},
+          #{datetime.day},
+          #{datetime.hour},
+          #{datetime.min}
+      ))).format("#{format}")))
+    });
+</script>
+<span id="#{span_id}"></span>
+HTML
+  end
 end
