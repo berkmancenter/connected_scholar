b9488395ec48cbfc0cc36fb0cf1e73297a36310a
Matt Smith <matt@mattsmith.me>
2011-10-03 02:42:20 -0500

    validate uniqueness of citation text per resource

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -3,4 +3,5 @@ class Citation < ActiveRecord::Base
   has_one :document, :through => :resource
 
   validates_uniqueness_of :default, :scope => :resource_id, :if => Proc.new {|citation| citation.default}
+  validates_uniqueness_of :citation_text, :scope => :resource_id
 end
