061aebafd9baa1713d968b68786f3b13abad5820
Joe Kutner <jpkutner@gmail.com>
2011-10-11 11:29:06 -0500

    improved external links

--- lib/search_util.rb
+++ lib/search_util.rb
@@ -10,9 +10,14 @@ module SearchUtil
         search_url += "&filter=#{f[:filter_type]}:#{CGI::escape(f[:filter])}"
       end
       Rails.logger.info "LC => #{search_url}"
-      x = JsonUtil::get_json search_url
+      items = JsonUtil::get_json search_url
       Rails.logger.info "LibraryCloud service returned a response"
-      x
+      return items.merge(
+          "docs" => items['docs'].map { |item|
+            item.merge('links' => (item['id_inst'] && item['id_inst'] != "") ?
+                [["Hollis", item_link(item['id_inst'])]] : [])
+          }
+      )
     end
   rescue RestClient::RequestTimeout => e
     return {
@@ -33,13 +38,7 @@ module SearchUtil
         # remove uni-code chars
         doc = Hpricot(html.gsub!(/[\x80-\xff]/, ""))
         return {
-            "docs" => (doc/"div.gs_r").map { |gs_r|
-              {
-                  "title" => (gs_r/"div.gs_rt").inner_text,
-                  "creator" => [(gs_r/"span.gs_a").inner_text.split('-').first],
-                  "desc_subject" => (gs_r/"font").inner_text
-              }
-            },
+            "docs" => (doc/"div.gs_r").map { |gs_r| parse_google_record(gs_r) },
             "limit" => limit,
             "start" => start,
             "num_found" => -1,
@@ -66,5 +65,29 @@ module SearchUtil
   def with_google
     yaml = YAML.load_file("#{Rails.root}/config/googlescholar.yml")
     yield yaml['url']
+  end
+
+  def parse_google_record(gs_r)
+    {
+        "title" => build_google_title(gs_r),
+        "creator" => build_google_creator(gs_r),
+        "desc_subject" => (gs_r/"font").inner_text,
+        "links" => build_google_links(gs_r)
+    }
+  end
+
+  def build_google_title(gs_r)
+    (gs_r/"div.gs_rt").inner_text.
+        gsub('[PDF] ', '').
+        gsub('[BOOK] ', '').
+        gsub('[CITATION] ', '')
+  end
+
+  def build_google_creator(gs_r)
+    [(gs_r/"span.gs_a").inner_text.split('-').first]
+  end
+
+  def build_google_links(gs_r)
+    ((a = (gs_r/"div.gs_rt > h3 > a")) && a.first) ? [["Google", a.first[:href]]] : []
   end
 end
