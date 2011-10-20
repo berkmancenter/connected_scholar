5018fc9eb1fd832fef79456605a615a2cca0455c
Matt Smith <matt@mattsmith.me>
2011-10-20 13:59:06 -0500

    remove unneeded spec

--- spec/lib/citation_factory_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe CitationFactory do
-  describe "#citation_formatter" do
-    it "should return MlaCitation for :mla type" do 
-      CitationFactory.citation_formatter(:mla).should == MlaCitation
-    end
-  end
-end
